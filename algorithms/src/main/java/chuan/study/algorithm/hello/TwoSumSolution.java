42dd5d439547634c0a88993351e3ebf156abf042
jiang.chuan <jiang@chuan.com>
2019-04-22 15:33:29 +0800

    feat: 增加算法与数据结构模块

---
+++ algorithms/src/main/java/chuan/study/algorithm/hello/TwoSumSolution.java
@@ -0,0 +1,45 @@
+package chuan.study.algorithm.hello;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/two-sum/
+ *
+ * @author chuan.jiang <jctr@qq.com>
+ * @version 1.0.0
+ * @date 2019-04-22
+ */
+public class TwoSumSolution {
+    /**
+     * 私有构造函数
+     */
+    private TwoSumSolution() { }
+
+    /**
+     * 求满足条件的下标
+     *
+     * @param nums   输入的数
+     * @param target 目标数
+     * @return 下票数组
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> indexMap = new HashMap<>((int) Math.ceil(nums.length / 0.75));
+        for (int i = 0; i < nums.length; i++) {
+            int remain = target - nums[i];
+            if (indexMap.containsKey(remain)) {
+                return new int[]{indexMap.get(remain), i};
+            }
+            indexMap.put(nums[i], i);
+        }
+        return new int[2];
+    }
+
+
+    public static void main(String[] args) {
+        int[] results = TwoSumSolution.twoSum(new int[]{2, 7, 11, 15}, 9);
+
+        System.out.println(Arrays.toString(results));
+    }
+}
