c6d2ab0339643dfad39fe68185c2cc41c84b6021
jiang.chuan <jiang@chuan.com>
2019-04-24 21:03:32 +0800

    feat: 增加响应式web接口

---
+++ spring-reactive/src/main/java/chuan/study/spring/reactive/ReactiveApplication.java
@@ -0,0 +1,16 @@
+package chuan.study.spring.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chuan.jiang <jctr@qq.com>
+ * @version 1.0.0
+ * @date 2019-04-24
+ */
+@SpringBootApplication
+public class ReactiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveApplication.class, args);
+    }
+}
